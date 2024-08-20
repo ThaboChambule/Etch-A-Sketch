3cf36a69642c303daac8cd15f05efbbb50aee9e5
Thabo <thabochambule1@gmail.com>
2024-08-20 11:11:06 +0200

    add event listener for pixelated trail

--- scripts/script.js
+++ scripts/script.js
@@ -1,8 +1,21 @@
-const grid = document.querySelector(".Container")
+const grid = document.querySelector(".Container");
 
-for(i = 0 ; i < 256; i++)
-{
-    const gridItem = document.createElement('div'); //creates new html element
-    gridItem.classList.add('grid-item'); //gives the element a class
-    grid.appendChild(gridItem) //used to add a new child element to parent element
+for (let i = 0; i < 256; i++) {
+  const gridItem = document.createElement("div"); //creates new html element
+  gridItem.classList.add("grid-item"); //gives the element a class
+  grid.appendChild(gridItem); //used to add a new child element to parent element
 }
+
+const gridItems = document.querySelectorAll(".grid-item");
+
+gridItems.forEach ( item => {
+    item.addEventListener("mouseover", () => {
+
+        item.style.backgroundColor = 'yellow' //changes background colour
+        
+
+
+        console.log("it works")
+    })
+})
+;
