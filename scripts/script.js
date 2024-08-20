54795140fdb28b1b98ae40b686123554127ffcaf
Thabo <thabochambule1@gmail.com>
2024-08-20 14:03:28 +0200

    Add new grid button

--- scripts/script.js
+++ scripts/script.js
@@ -1,21 +1,33 @@
-const grid = document.querySelector(".Container");
+num = 256;
 
-for (let i = 0; i < 256; i++) {
-  const gridItem = document.createElement("div"); //creates new html element
-  gridItem.classList.add("grid-item"); //gives the element a class
-  grid.appendChild(gridItem); //used to add a new child element to parent element
-}
+getGrid(num);
 
-const gridItems = document.querySelectorAll(".grid-item");
+function getGrid(num) {
+  const grid = document.querySelector(".Container");
+  grid.innerHTML = "";
 
-gridItems.forEach ( item => {
+  for (let i = 0; i < num; i++) {
+    const gridItem = document.createElement("div"); //creates new html element
+    gridItem.classList.add("grid-item"); //gives the element a class
+    grid.appendChild(gridItem); //used to add a new child element to parent element
+  }
+
+  const gridItems = document.querySelectorAll(".grid-item");
+
+  gridItems.forEach((item) => {
     item.addEventListener("mouseover", () => {
-
-        item.style.backgroundColor = 'yellow' //changes background colour
-        
-
-
-        console.log("it works")
-    })
-})
-;
+      item.style.backgroundColor = "yellow"; //changes background colour
+    });
+  });
+}
+const button = document.querySelector(".button");
+button.addEventListener("click", () => {
+  let userInput = prompt("Please enter the number of squares per side"); //gets user input
+  if (userInput > 0 && userInput <= 100) {
+    let num = userInput * userInput;
+    getGrid(num);
+  }
+  else{
+    alert("Please enter value between 0 and 100")
+  }
+});
